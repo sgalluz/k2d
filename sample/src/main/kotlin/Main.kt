be1d7969040f0e349e233850f071885b33d8599f
Stefano Galluzzi <sgalluz@users.noreply.github.com>
2026-01-03 22:21:03 +0100

    feat(Main): update sample for using render system

--- sample/src/main/kotlin/Main.kt
+++ sample/src/main/kotlin/Main.kt
@@ -5,9 +5,8 @@ import androidx.compose.ui.window.application
 import io.sgalluz.k2d.ecs.*
 import io.sgalluz.k2d.ecs.systems.MovementSystem
 import io.sgalluz.k2d.rendering.k2dCanvas
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
 import io.sgalluz.k2d.ecs.systems.BoundarySystem
+import io.sgalluz.k2d.rendering.systems.ShapeRenderSystem
 
 fun main() = application {
     // 1. Initialize the World and Systems
@@ -24,26 +23,18 @@ fun main() = application {
         }
     }
 
+    // 3. Initialize the rendering systems (outside the World)
+    val renderer = remember { ShapeRenderSystem() }
+
     Window(onCloseRequest = ::exitApplication, title = "K2D Engine - ECS in Action") {
         k2dCanvas(
             onUpdate = { deltaTime ->
-                // 3. The engine heartbeat now drives the World
+                // 4. The engine heartbeat now drives the World
                 world.update(deltaTime)
             },
             onRender = {
-                // 4. For now, we render manually, but soon we'll have a RenderSystem
-                world.getEntities().forEach { entity ->
-                    val pos = entity.get<Position>()
-                    val sprite = entity.get<Sprite>()
-
-                    if (pos != null && sprite != null) {
-                        drawRect(
-                            color = sprite.color,
-                            topLeft = Offset(pos.x, pos.y),
-                            size = Size(sprite.size, sprite.size)
-                        )
-                    }
-                }
+                // 5. Let's pass the entities to the renderer manually
+                renderer.render(world.getEntities(), this)
             }
         )
     }
