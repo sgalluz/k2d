f09a4453a9724df57d5d33e805901fed74774c39
Stefano Galluzzi <sgalluz@users.noreply.github.com>
2026-01-05 22:20:00 +0100

    test(CollisionSystemTest): increase coverage for the collision system

--- engine/src/test/kotlin/io/sgalluz/k2d/ecs/systems/CollisionSystemTest.kt
+++ engine/src/test/kotlin/io/sgalluz/k2d/ecs/systems/CollisionSystemTest.kt
@@ -12,12 +12,10 @@ class CollisionSystemTest {
         val world = World()
         val system = CollisionSystem()
 
-        // Entity 1: at (0,0) with size 50x50
         val e1 = world.createEntity()
             .add(Position(0f, 0f))
             .add(BoxCollider(width = 50f, height = 50f))
 
-        // Entity 2: at (40,40) with size 50x50 (Overlaps e1)
         val e2 = world.createEntity()
             .add(Position(40f, 40f))
             .add(BoxCollider(width = 50f, height = 50f))
@@ -33,17 +31,101 @@ class CollisionSystemTest {
         val world = World()
         val system = CollisionSystem()
 
-        val e1 = world.createEntity()
-            .add(Position(0f, 0f))
-            .add(BoxCollider(width = 10f, height = 10f))
-
-        val e2 = world.createEntity()
-            .add(Position(100f, 100f))
-            .add(BoxCollider(width = 10f, height = 10f))
+        val e1 = world.createEntity().add(Position(0f, 0f)).add(BoxCollider(10f, 10f))
+        val e2 = world.createEntity().add(Position(100f, 100f)).add(BoxCollider(10f, 10f))
 
         system.update(world.getEntities(), 0.016f)
 
-        assertFalse(e1.get<BoxCollider>()!!.isColliding, "Entity 1 should not be colliding")
-        assertFalse(e2.get<BoxCollider>()!!.isColliding, "Entity 2 should not be colliding")
+        assertFalse(e1.get<BoxCollider>()!!.isColliding)
+        assertFalse(e2.get<BoxCollider>()!!.isColliding)
+    }
+
+    @Test
+    fun `should not collide when target is to the LEFT of source`() {
+        val world = World()
+        val system = CollisionSystem()
+        // e1 at (100, 100), e2 far to the left at (0, 100)
+        val e1 = world.createEntity().add(Position(100f, 100f)).add(BoxCollider(50f, 50f))
+        val e2 = world.createEntity().add(Position(0f, 100f)).add(BoxCollider(50f, 50f))
+
+        system.update(world.getEntities(), 0.016f)
+        assertFalse(e1.get<BoxCollider>()!!.isColliding)
+    }
+
+    @Test
+    fun `should not collide when target is to the RIGHT of source`() {
+        val world = World()
+        val system = CollisionSystem()
+        // e1 at (0, 0), e2 far to the right at (100, 0)
+        val e1 = world.createEntity().add(Position(0f, 0f)).add(BoxCollider(50f, 50f))
+        val e2 = world.createEntity().add(Position(100f, 0f)).add(BoxCollider(50f, 50f))
+
+        system.update(world.getEntities(), 0.016f)
+        assertFalse(e1.get<BoxCollider>()!!.isColliding)
+    }
+
+    @Test
+    fun `should not collide when target is ABOVE source`() {
+        val world = World()
+        val system = CollisionSystem()
+        // e1 at (100, 100), e2 far above at (100, 0)
+        val e1 = world.createEntity().add(Position(100f, 100f)).add(BoxCollider(50f, 50f))
+        val e2 = world.createEntity().add(Position(100f, 0f)).add(BoxCollider(50f, 50f))
+
+        system.update(world.getEntities(), 0.016f)
+        assertFalse(e1.get<BoxCollider>()!!.isColliding)
+    }
+
+    @Test
+    fun `should not collide when target is BELOW source`() {
+        val world = World()
+        val system = CollisionSystem()
+        // e1 at (0, 0), e2 far below at (0, 100)
+        val e1 = world.createEntity().add(Position(0f, 0f)).add(BoxCollider(50f, 50f))
+        val e2 = world.createEntity().add(Position(0f, 100f)).add(BoxCollider(50f, 50f))
+
+        system.update(world.getEntities(), 0.016f)
+        assertFalse(e1.get<BoxCollider>()!!.isColliding)
+    }
+
+    @Test
+    fun `should ignore entities missing Position or BoxCollider`() {
+        val world = World()
+        val system = CollisionSystem()
+
+        world.createEntity().add(Position(0f, 0f)) // Only Position
+        world.createEntity().add(BoxCollider(50f, 50f)) // Only Collider
+
+        val e3 = world.createEntity().add(Position(100f, 100f)).add(BoxCollider(10f, 10f))
+
+        system.update(world.getEntities(), 0.016f)
+        assertFalse(e3.get<BoxCollider>()!!.isColliding, "System should still run correctly for valid entities")
+    }
+
+    @Test
+    fun `should reset isColliding even for entities without Position`() {
+        val world = World()
+        val system = CollisionSystem()
+
+        val collider = BoxCollider(50f, 50f, isColliding = true)
+        world.createEntity().add(collider) // Missing Position, but should be reset
+
+        system.update(world.getEntities(), 0.016f)
+
+        assertFalse(collider.isColliding, "isColliding should be reset to false during update")
+    }
+
+    @Test
+    fun `should reset isColliding to false when entities move apart`() {
+        val world = World()
+        val system = CollisionSystem()
+        val collider = BoxCollider(50f, 50f, isColliding = true)
+
+        world.createEntity().add(Position(0f, 0f)).add(collider)
+        world.createEntity().add(Position(200f, 200f)).add(BoxCollider(50f, 50f))
+
+        system.update(world.getEntities(), 0.016f)
+
+        assertFalse(collider.isColliding, "Should reset to false if no longer touching")
     }
 }
