384108baac28b97dee9897a79d346c2df03bba44
Stefano Galluzzi <14968717+sgalluz@users.noreply.github.com>
2026-01-15 00:46:49 +0100

    feat(build.gradle.kts): make version resolution automatic

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,21 @@ plugins {
     id("org.jetbrains.compose") version "1.5.11" apply false
 }
 
+val versionProps = rootProject.file("version.properties")
+    .takeIf { it.exists() }
+    ?.readLines()
+    ?.associate {
+        val (k, v) = it.split("=")
+        k to v
+    }
+
+val major = versionProps?.get("major") ?: "0"
+val minor = versionProps?.get("minor") ?: "0"
+
 allprojects {
     group = "io.github.sgalluz.k2d"
     version = findProperty("versionName") as String?
-        ?: "0.0.1-SNAPSHOT"
+        ?: "$major.$minor.0-SNAPSHOT"
 
     repositories {
         google()
