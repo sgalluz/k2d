6062de120ee133016d7d1ccfb45e307a9064bd9b
Stefano Galluzzi <sgalluz@users.noreply.github.com>
2026-01-23 22:03:03 +0100

    feat(release): add first version of release workflow (without signing keys)

--- .github/scripts/run-gradle.sh
+++ .github/scripts/run-gradle.sh
@@ -2,16 +2,17 @@
 set -e
 
 RAW_TASKS=$1
-PUBLISH=$2
-VERSION=$3
+VERSION=$2
 
 CLEAN_TASKS="${RAW_TASKS//:engine:/}"
 TASKS=$(printf ':engine:%s ' $CLEAN_TASKS)
 
 [[ "$CLEAN_TASKS" == *"test"* ]] && TASKS=":engine:cleanTest $TASKS"
 
-VERSION_ARG=""
-[[ "$PUBLISH" == "true" ]] && VERSION_ARG="-PversionName=${VERSION}"
+if [[ -n "$VERSION" ]]; then
+  export VERSION
+  echo "Using VERSION=$VERSION"
+fi
 
-echo "Executing: ./gradlew $TASKS $VERSION_ARG --build-cache --configuration-cache"
-./gradlew $TASKS $VERSION_ARG --build-cache --configuration-cache
+echo "Executing: ./gradlew $TASKS --build-cache --configuration-cache"
+./gradlew $TASKS --build-cache --configuration-cache
